e468c61aede5d3a198d4d848d9fc57c29a70d955
Pete Cornish <outofcoffee@gmail.com>
2021-10-01 02:36:46 +0100

    docs: adds second TypeScript file to sample to demonstrate import.

--- sample/src/index.ts
+++ sample/src/index.ts
@@ -1,19 +1,19 @@
-import {context, logger, respond} from "@imposter-js/types";
+import {context, logger, respond} from "@imposter-js/types"
+import {generateName} from "./names"
 
-console.info(`context: ${context}`);
+console.info(`context: ${context}`)
 
-const request = context.request;
+const request = context.request
 
 if (request.headers['User-Agent'] === 'bad-actor') {
-    respond().withStatusCode(400);
-}
+    respond().withStatusCode(400)
 
-const body = JSON.stringify({
-    "id": 'foo',
-});
-
-if (request.method === 'POST') {
-    respond().withStatusCode(201).withData(body);
 } else {
-    respond().withStatusCode(200).withData(body);
+    const body = JSON.stringify({
+        "name": generateName(),
+    })
+
+    respond()
+        .withStatusCode(200)
+        .withData(body)
 }
